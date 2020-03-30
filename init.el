6b5b6f758b8550708f3ace489cb1eedc3982018d
Adrien Dessy <adrien.dessy@gmail.com>
2020-03-30 14:02:21 +0200

    moving org-contacts config

--- init.el
+++ init.el
@@ -306,13 +306,7 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
-  (org-babel-load-file (expand-file-name "dotspacemacs.org" dotspacemacs-directory))
-  (use-package org-contacts
-    :ensure nil
-    :after org
-    :custom (org-contacts-files '("~/org/contacts.org")))
-  )
-
+  (org-babel-load-file (expand-file-name "dotspacemacs.org" dotspacemacs-directory)))
 ;; Do not write anything past this comment. This is where Emacs will
 ;; auto-generate custom variable definitions.
 (custom-set-variables
