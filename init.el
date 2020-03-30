3f3ef2e61e263321e14b3ca489048c099d60ee84
Adrien Dessy <adrien.dessy@gmail.com>
2020-03-30 17:37:01 +0200

    move org-capture templates

--- init.el
+++ init.el
@@ -314,23 +314,6 @@ you should place your code here."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files
-   (quote
-    ("~/Library/Mobile Documents/iCloud~com~appsonthemove~beorg/Documents/org")))
- '(org-capture-templates
-   (quote
-    (("j" "Journal entry" entry
-      (file+olp+datetree "~/org/journal.org")
-      (file "~/org/capture_templates/journal.template")
-      :empty-lines 1)
-     ("c" "New contact" entry
-      (file "~/org/contacts.org")
-      (file "~/org/capture_templates/contact.template")
-      :empty-lines 1)
-     ("w" "Web capture" entry
-      (file+olp+datetree "~/org/capture.org")
-      (file "~/org/capture_templates/firefox.template")
-      :empty-lines 1))))
  '(org-modules
    (quote
     (org-bbdb org-bibtex org-docview org-gnus org-info org-irc org-mhe org-protocol org-rmail org-w3m org-drill)))
